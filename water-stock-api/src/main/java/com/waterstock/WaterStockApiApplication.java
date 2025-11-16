e7d6401d4ac7e01fbf071decd404b728f923135a
Popopond <115360402+Popopond@users.noreply.github.com>
2025-11-16 23:04:21 +0700

    initial project

---
+++ water-stock-api/src/main/java/com/waterstock/WaterStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.waterstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterStockApiApplication.class, args);
+	}
+
+}
---
+++ water-stock-api/src/test/java/com/waterstock/WaterStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waterstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
